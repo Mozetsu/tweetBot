50c9ed981864aab45b7e166c1dbbf7937544d593
Mozetsu <code.moz.00@gmail.com>
2020-06-17 19:36:59 +0100

    add favorite feature

--- utils/tweet.js
+++ utils/tweet.js
@@ -12,10 +12,15 @@ const result = (error, data, response) => {
 
 const tweet = (tweet) => T.post('statuses/update', { status: tweet }, result);
 
+// const retweet = (id) => T.post(`statuses/retweet/${id}`, result);
 const retweet = (id) => T.post(`statuses/retweet/${id}`, result);
+const favorite = (id) => T.post(`favorites/create`, { id }, result);
 
 const reply = (id, reply) => {
 	T.post('statuses/update', { in_reply_to_status_id: id, auto_populate_reply_metadata: true, status: reply }, result);
 };
 
-retweet('1267703699729780736');
+// T.stream('statuses/filter', { track: 'javascript' }, (stream) => {
+// 	stream.on('data', (tweet) => console.log(tweet));
+// 	stream.on('error', (error) => console.log(error));
+// });
