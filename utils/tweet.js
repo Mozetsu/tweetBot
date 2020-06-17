12dddff1fa8672eb6b842dd249af98a15de64a64
Mozetsu <code.moz.00@gmail.com>
2020-06-17 18:55:21 +0100

    add environment variables

--- utils/tweet.js
+++ utils/tweet.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const Twitter = require('twitter');
-const auth = require('../config/GhownAuth');
+const auth = require('../config/tokens');
 
 // API auth
 const T = new Twitter(auth);
@@ -14,3 +15,5 @@ const tweet = (tweet) => T.post('statuses/update', { status: tweet }, result);
 const reply = (id, reply) => {
 	T.post('statuses/update', { in_reply_to_status_id: id, auto_populate_reply_metadata: true, status: reply }, result);
 };
+
+reply('1273312848022831104', "forgot the -D flag");
