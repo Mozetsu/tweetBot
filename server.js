4a7f663fdf345395995dd85c492838c8f0dcb834
Mozetsu <code.moz.00@gmail.com>
2020-07-10 19:05:45 +0100

    Add express

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-const { tweet, result } = require('./utils/twitter');
-const fetch = require('node-fetch');
-var fs = require('fs');
+const { tweetRandomFact } = require('./utils/twitter');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+const interval = 1000 * 30; // 3 hours
 
-const interval = 1000 * 3600 * 3; // 3 hours
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-setInterval(tweetRandomFact, interval);
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/utils/database.json`);
+});
 
-async function tweetRandomFact() {
-	try {
-		// parse json file into a variable
-		const database = JSON.parse(fs.readFileSync('./utils/database.json'));
+// setInterval(tweetRandomFact, interval);
 
-		// fetch random fact
-		const response = await fetch('https://uselessfacts.jsph.pl//random.json?language=en');
-		const { id, text } = await response.json();
-
-		// if fact already tweeted, get new one
-		if (database.ids.includes(id)) return tweetRandomFact();
-
-		// save fact id on json file
-		database.ids.push(id);
-		await fs.writeFile('./utils/database.json', JSON.stringify(database), result);
-
-		// tweet
-		await tweet(text);
-	} catch (err) {
-		console.log(err);
-	}
-}
+app.listen(PORT, () => console.log(`// ${PORT}`));
--- utils/twitter.js
+++ utils/twitter.js
@@ -1,4 +1,6 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const fetch = require('node-fetch');
+const fs = require('fs');
 const TwitterLite = require('twitter-lite');
 const auth = require('../config/auth');
 
@@ -10,4 +12,27 @@ const result = (err, data, response) => {
 
 const tweet = (tweet) => Twitter.post('statuses/update', { status: tweet }, result);
 
-module.exports = { tweet, result };
+async function tweetRandomFact() {
+	try {
+		// parse json file into a variable
+		const database = JSON.parse(fs.readFileSync('./utils/database.json'));
+
+		// fetch random fact
+		const response = await fetch('https://uselessfacts.jsph.pl//random.json?language=en');
+		const { id, text } = await response.json();
+
+		// if fact already tweeted, get new one
+		if (database.ids.includes(id)) return tweetRandomFact();
+
+		// save fact id on json file
+		database.ids.push(id);
+		await fs.writeFile('./utils/database.json', JSON.stringify(database), result);
+
+		// tweet
+		await tweet(text);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+module.exports = { tweetRandomFact };
