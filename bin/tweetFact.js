da66cf8376f855ce79d4b84369864e4f9e646b9e
Mozetsu <code.moz.00@gmail.com>
2020-07-11 10:37:52 +0100

    Add bin for heroku scheduler

---
+++ bin/tweetFact.js
@@ -0,0 +1,3 @@
+const { tweetRandomFact } = require('../utils/twitter');
+
+tweetRandomFact();
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const { tweetRandomFact } = require('./utils/twitter');
 const express = require('express');
 const app = express();
 const databaseRoute = require('./routes/database');
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', databaseRoute);
-
-// const interval = 1000 * 3600 * 3; // 3 Hours
-// setInterval(() => tweetRandomFact(), interval);
-tweetRandomFact();
 
 app.listen(PORT, () => console.log(`// ${PORT}`));
