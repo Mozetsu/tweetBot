a6c2e122619ecabea71aadd416b1589df6b698f1
Mozetsu <code.moz.00@gmail.com>
2020-07-12 13:21:40 +0100

    Heroku worker test #7

--- bin/clock.js
+++ bin/clock.js
@@ -1,6 +1,7 @@
 const CronJob = require('cron').CronJob;
 const amqp = require('amqp-connection-manager');
 
+console.log(process.env.CLOUDAMQP_URL);
 const AMQP_URL = process.env.CLOUDAMQP_URL || 'amqp://localhost';
 if (!AMQP_URL) process.exit(1);
 
@@ -12,7 +13,7 @@ const JOBS = [
 		name: 'Cron process 1',
 		message: { taskName: 'tweetRandomFact', queue: 'worker-queue' }, // message in json format
 		// cronTime: '0 0 10-23/3 * * *', // Every 3h between 10am and 11pm
-		cronTime: '0 */2 11-13 * * *', // Every 2min between 11am and 12pm
+		cronTime: '0 */1 11-14 * * *', // Every 2min between 11am and 12pm
 		repeat: 1,
 	},
 	{
