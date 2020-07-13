632642a29e8cac9510d2a8123127b4b3a0ee276b
Mozetsu <code.moz.00@gmail.com>
2020-07-13 13:19:23 +0100

    Add route for tweetRandomFact

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { tweetRandomFact } = require('./utils/tweetBot');
+
+router.get('/tweetRandomFact', (req, res) => {
+	tweetRandomFact();
+	res.status(200).send({ server: 'tweeted!' });
+});
 
 router.get('/database', (req, res) => {
 	res.sendFile(path.join(__dirname + '/../utils/database.json'));
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const databaseRoute = require('./routes/database');
+const routes = require('./routes/database');
+
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', databaseRoute);
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`// ${PORT}`));
