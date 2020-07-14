2650c6d6912f1ff195738f5b41f35e6d4caa3e4f
Mozetsu <code.moz.00@gmail.com>
2020-07-14 02:04:23 +0100

    Add router (again)

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const tweetBot = require('./utils/tweetBot');
+
+router.get('/randomFact', (req, res) => {
+	tweetBot.randomFact();
+	res.status(200).send({ fact: 'tweeted!' });
+});
+
+router.get('/mostPlayedTrack', (req, res) => {
+	tweetBot.mostPlayedTrack();
+	res.status(200).send({ track: 'tweeted!' });
+});
+
+router.get('/database', (req, res) => {
+	res.sendFile(path.join(__dirname + '/../utils/database.json'));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const tweetBot = require('./utils/tweetBot');
+const routes = require('./routes/routes');
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/randomFact', (req, res) => {
-	tweetBot.randomFact();
-	res.status(200).send({ fact: 'tweeted!' });
-});
-
-app.get('/mostPlayedTrack', (req, res) => {
-	tweetBot.mostPlayedTrack();
-	res.status(200).send({ track: 'tweeted!' });
-});
-
-app.get('/database', (req, res) => {
-	res.sendFile(path.join(__dirname + './utils/database.json'));
-});
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`// ${PORT}`));
